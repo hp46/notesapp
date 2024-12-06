979dfe2eb7d47821d2eaa6be9641327ad4a49d54
Hyungseok (Jason) Park <hyungseokp419@gmail.com>
2024-12-06 15:23:54 -0500

    rabat page

---
+++ src/pages/Rabat.jsx
@@ -0,0 +1,41 @@
+// import { useState, useEffect } from "react";
+import './index.css'
+// import {
+//   Authenticator,
+//   TextAreaField,
+//   Button,
+//   Text,
+//   TextField,
+//   Heading,
+//   Flex,
+//   View,
+//   SelectField,
+//   SearchField,
+//   Grid,
+//   PhoneNumberField,
+//   Divider,
+//   Table,
+//   TableCell,
+//   TableBody,
+//   TableHead,
+//   TableRow,
+//   RadioGroupField,
+//   Radio,
+// } from "@aws-amplify/ui-react";
+// import { Amplify } from "aws-amplify";
+// import "@aws-amplify/ui-react/styles.css";
+// import { getUrl } from "aws-amplify/storage";
+// import { uploadData } from "aws-amplify/storage";
+// import { generateClient } from "aws-amplify/data";
+// import outputs from "../amplify_outputs.json";
+/**
+ * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
+ */
+
+
+
+export default function Rabat() {
+    return (
+        <div>testing this page</div>
+    );
+}
