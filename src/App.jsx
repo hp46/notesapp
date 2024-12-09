f0de3e16802180dc52d5295527124cda856ae1c0
Hyungseok (Jason) Park <hyungseokp419@gmail.com>
2024-12-09 14:37:16 -0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ export default function App() {
         <Flex>
           <Image
           alt="Morocco Map"
-          src="/morocco.svg"
+          src="/morocco.png"
           objectPosition="50% 50%"
           backgroundColor="initial"
           height="75%"
