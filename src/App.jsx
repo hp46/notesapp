7f17b5fc27d709d25cef89e13732970129050602
Hyungseok (Jason) Park <hyungseokp419@gmail.com>
2024-12-11 21:09:26 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import {
   Route,
 } from 'react-router-dom';
   import "@aws-amplify/ui-react/styles.css";
-  import image from "./assets/morocco1.png"
+  import image from "./assets/morooco1.png"
 /**
  * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
  */
