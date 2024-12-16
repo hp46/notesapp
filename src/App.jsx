a39606acb64c2dbe98fcf4718b13ac9fe3aaa106
Hyungseok (Jason) Park <hyungseokp419@gmail.com>
2024-12-16 15:08:13 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ export default function App() {
                 Sign Out
               </Button>
             </div>
-            <div className="flex w-[30%] h-[7%] absolute top-[40%] left-[50%] items-center justify-start">
+            <div className="flex w-[30%] h-[7%] absolute top-[40%] left-[50%]">
             <ReactRouterLink to="/Rabat" component={Link}>
               <div className="flex flex-row h-full">
               <Image
