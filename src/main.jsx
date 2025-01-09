25d44c389265c219dbdf494715d56a2bb7afe247
Hyungseok (Jason) Park <hyungseokp419@gmail.com>
2025-01-09 14:39:04 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import Rabat from './pages/Rabat.jsx'
 import AlHaouz from './pages/AlHaouz.jsx'
+import Marrakech from './pages/Marrakech.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
   {path: "/", element: <App />},
   {path: "/rabat", element: <Rabat />},
   {path: "/alhaouz", element: <AlHaouz />}
+  {path: "/marrakech", element: <Marrakech />}
 ]);
 
 createRoot(document.getElementById('root')).render(
