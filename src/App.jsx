6be28831ca3b944b875155136fac625cb2e25cdd
Hyungseok (Jason) Park <hyungseokp419@gmail.com>
2025-01-13 16:52:51 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -53,8 +53,10 @@ export default function App() {
               <div className="absolute top-40 left-10 border-2 rounded-lg p-10 w-80 border-black text-black">
                 <h1 className="text-2xl uppercase font-black">Choose a City</h1>
                 <ReactRouterLink  className=" flex hover:font-black hover:italic hover:w-1/2" to="/Rabat" component={Link}>  
-                  <p className="text-xl text-black">Rabat</p>
-                    <div className="absolute top-[110%] left-[322%] w-10 hover:w-20">
+                  <div>
+                    <p className="text-xl text-black">Rabat</p>
+                  </div>
+                  <div className="absolute top-[110%] left-[322%]">
                       <Image
                         alt="pin"
                         src={pin}
