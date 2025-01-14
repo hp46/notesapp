20ea562d1346fc55244e6452134d763c35502bd6
Hyungseok (Jason) Park <hyungseokp419@gmail.com>
2025-01-14 14:54:28 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ export default function App() {
                   </div>
                   <div className="absolute top-[110%] left-[322%]">
                     <button className="rounded-full bg-[#f05829] hover:bg-[#467830]">
-                      <p className=" ">Rabat</p>
+                      <p className="invisible">Rabat</p>
                     </button>
                   </div>
                 </ReactRouterLink>
